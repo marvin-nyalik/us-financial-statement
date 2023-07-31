ee8e7f0586a678f28b2334fa3841ec99431a0d7d
MARVIN <o.silverygold@gmail.com>
2023-07-31 23:19:24 +0300

    Add redux slice for companies

---
+++ src/redux/company/companySlice.js
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  loading: false,
+  report: [],
+  error: null,
+};
+
+export const fetchReport = createAsyncThunk('report/fetch',
+  async (symbol) => {
+    const baseURL = 'https://financialmodelingprep.com/api/v3/income-statement/';
+    const symbolID = symbol;
+    const endURL = '?limit=120&apikey=c5854d756f7d6f0e8b7e68348830a2aa';
+    const fetchURL = `${baseURL}${symbolID}${endURL}`;
+    const report = await axios.get(fetchURL);
+    const selectedReport = report.data.map((report) => ({
+      date: report.date,
+      currency: report.reportedCurrency,
+      year: report.calendarYear,
+      researchExpenses: report.researchAndDevelopmentExpenses,
+      income: report.operatingIncome,
+    }));
+
+    return selectedReport;
+  });
+
+const companySlice = createSlice({
+  name: 'company',
+  initialState,
+  reducers: {
+    clearReports: (state) => {
+      state.report = [];
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchReport.pending, (state) => {
+        state.loading = true;
+      })
+
+      .addCase(fetchReport.fulfilled, (state, action) => {
+        state.loading = false;
+        state.report = action.payload;
+      })
+
+      .addCase(fetchReport.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { clearReports } = companySlice.actions;
+export default companySlice.reducer;
