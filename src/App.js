1b2c4d8ef40eea9be7edda8d2347c8742455fc9c
MARVIN <o.silverygold@gmail.com>
2023-07-31 23:23:16 +0300

    Add redux store provider

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Companies from './components/Companies';
+import Detail from './components/Detail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Companies />} />
+          <Route path="/detail/:symbol" element={<Detail />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
