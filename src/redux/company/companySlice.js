95b0e44e10c81413753eddc915efeecf8355106a
MARVIN <o.silverygold@gmail.com>
2023-08-02 17:39:14 +0300

    Update API key

--- src/redux/company/companySlice.js
+++ src/redux/company/companySlice.js
@@ -11,7 +11,7 @@ export const fetchReport = createAsyncThunk('report/fetch',
   async (symbol) => {
     const baseURL = 'https://financialmodelingprep.com/api/v3/income-statement/';
     const symbolID = symbol;
-    const endURL = '?limit=120&apikey=c5854d756f7d6f0e8b7e68348830a2aa';
+    const endURL = '?limit=120&apikey=1b41a7ddd511d92ce0de893c4f689169';
     const fetchURL = `${baseURL}${symbolID}${endURL}`;
     const report = await axios.get(fetchURL);
     const selectedReport = report.data.map((report) => ({
